4c3227e460e06a5e07d30b0fb381f27b047dc1b1
Bruno Figueiredo Sousa <brunofigueiredo11@hotmail.com.br>
2022-10-17 00:09:49 -0300

    atualizacao

--- src/main/java/com/api/scania/api/scania/controller/LicitacaoController.java
+++ src/main/java/com/api/scania/api/scania/controller/LicitacaoController.java
@@ -3,17 +3,11 @@ package com.api.scania.api.scania.controller;
 import com.api.scania.api.scania.dto.EmailDto;
 import com.api.scania.api.scania.model.*;
 import com.api.scania.api.scania.repository.*;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,8 +16,6 @@ import java.util.List;
 public class LicitacaoController {
     @Autowired
     private LicitacaoRepository licitacaoRepository;
-    @Autowired
-    private LicitacaoAdapterRepository licitacaoAdapterRepository;
     @Autowired
     private StatusRepository statusRepository;
     @Autowired
@@ -39,10 +31,10 @@ public class LicitacaoController {
     private OrgaoController orgController;
 
     @GetMapping("/licitacao")
-    public List<LicitacaoAdapter> lista() {
+    public List<Licitacao> lista() {
 
 
-        return licitacaoAdapterRepository.findAllLicAdapter();
+        return licitacaoRepository.findAllLicAdapter();
     }
 
     @GetMapping("/licitacao/{id}")
@@ -57,31 +49,27 @@ public class LicitacaoController {
             List<Licitacao> lic = lista(id);
             licitacaoRepository.updateStatus(status, id);
             if (lic.get(0).getCd_status() == 2 && status == 1 || lic.get(0).getCd_status() == 3 && status == 1) {
-                List<Descricao> desc = descController.lista(lic.get(0).getCd_descricao());
-                List<Orgao> orgao = orgController.lista(lic.get(0).getCd_orgao());
                 emailController.sendEmail(new EmailDto("brunofigueiredo1120@gmail.com",
                         "brunofigueiredo1120@gmail.com",
-                         "Licitação Selecionada - Edital - " + lic.get(0).getCd_edital(),
-                           "Licitação Selecionada: Edital - " + lic.get(0).getCd_edital() +
-                                "\nOrgão - " + orgao.get(0).getOrgao() +
+                        "Licitação Selecionada - Edital - " + lic.get(0).getCd_edital(),
+                        "Licitação Selecionada: Edital - " + lic.get(0).getCd_edital() +
+                                "\nOrgão - " + lic.get(0).getOrgao() +
                                 "\nData de Acolhimento - " + lic.get(0).getDt_acolhimento() +
                                 "\nData de Disputa - " + lic.get(0).getDt_disputa() +
-                                "\nDescrição - " + desc.get(0).getTexto() +
-                                "\nLink para o Edital - " + desc.get(0).getEdital_link(),
+                                "\nDescrição - " + lic.get(0).getTexto() +
+                                "\nLink para o Edital - " + lic.get(0).getEdital_link(),
                         "00/00/0000",
                         StatusEmail.SEND));
-            }  else if (lic.get(0).getCd_status() == 1 && status == 3) {
-                List<Descricao> desc = descController.lista(lic.get(0).getCd_descricao());
-                List<Orgao> orgao = orgController.lista(lic.get(0).getCd_orgao());
+            } else if (lic.get(0).getCd_status() == 1 && status == 3) {
                 emailController.sendEmail(new EmailDto("brunofigueiredo1120@gmail.com",
                         "brunofigueiredo1120@gmail.com",
                         "Alteração ****** Licitação *** DESCARTADA *** Edital - " + lic.get(0).getCd_edital(),
                         "Alteração ****** Licitação DESCARTADA: Edital - " + lic.get(0).getCd_edital() +
-                                "\nOrgão - " + orgao.get(0).getOrgao() +
+                                "\nOrgão - " + lic.get(0).getOrgao() +
                                 "\nData de Acolhimento - " + lic.get(0).getDt_acolhimento() +
                                 "\nData de Disputa - " + lic.get(0).getDt_disputa() +
-                                "\nDescrição - " + desc.get(0).getTexto() +
-                                "\nLink para o Edital - " + desc.get(0).getEdital_link(),
+                                "\nDescrição - " + lic.get(0).getTexto() +
+                                "\nLink para o Edital - " + lic.get(0).getEdital_link(),
                         "00/00/0000",
                         StatusEmail.SEND));
             }
@@ -90,5 +78,10 @@ public class LicitacaoController {
             return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
         }
 
+    }
+
+    @GetMapping("/licitacao/select/{status}")
+    public List<Licitacao> listFromStatus(@PathVariable int status) {
+        return licitacaoRepository.selectFromStatus(status);
     }
 }
